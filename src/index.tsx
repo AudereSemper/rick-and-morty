5af8f75f867f15cfd000f62ed07c2362782d0dc4
Federico Castorina <federico.castorina@mikamai.com>
2021-01-23 23:42:25 +0100

    deleted starter files

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { store } from 'src/redux/store';
-import './index.css';
 import { Provider } from 'react-redux';
 import App from 'src/App';
 import * as serviceWorker from './serviceWorker';
